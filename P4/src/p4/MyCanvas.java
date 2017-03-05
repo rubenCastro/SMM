a659319a24d7ea7f57100063984a1ae1fb8b57a1
Juan Antonio <corteshxn@gmail.com>
2017-03-05 18:53:20 +0100

    Oval and square.

--- P4/src/p4/MyCanvas.java
+++ P4/src/p4/MyCanvas.java
@@ -6,10 +6,13 @@
 package p4;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.LayoutManager;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JPanel;
-import org.w3c.dom.events.MouseEvent;
+import p4.MainWindow.FIGURE_TYPE;
 
 /**
  *
@@ -18,20 +21,17 @@ import org.w3c.dom.events.MouseEvent;
 public class MyCanvas extends JPanel implements MouseListener {
 
     private Color activeColor;
-    private MyFigure activeFigure;
-    private String activeType;
-    private Boolean isFilled;
+    private MyFigure activeFigure = new MyFigure();
+    private FIGURE_TYPE activeType;
+    private Boolean isFilled = false;
 
-    @Override
-    public void mouseClicked(java.awt.event.MouseEvent e) {
-
+    public MyCanvas() {
+        super();
+        this.setBackground(Color.WHITE);
+        this.setPreferredSize(new Dimension(600, 500));
     }
 
-    public MyCanvas(LayoutManager layout) {
-        super(layout);
-    }
-
-    public MyCanvas(Color activeColor, MyFigure activeFigure, String activeType, Boolean isFilled, LayoutManager layout) {
+    public MyCanvas(Color activeColor, MyFigure activeFigure, FIGURE_TYPE activeType, Boolean isFilled, LayoutManager layout) {
         super(layout);
         this.activeColor = activeColor;
         this.activeFigure = activeFigure;
@@ -55,11 +55,11 @@ public class MyCanvas extends JPanel implements MouseListener {
         this.activeFigure = activeFigure;
     }
 
-    public String getActiveType() {
+    public FIGURE_TYPE getActiveType() {
         return activeType;
     }
 
-    public void setActiveType(String activeType) {
+    public void setActiveType(FIGURE_TYPE activeType) {
         this.activeType = activeType;
     }
 
@@ -72,23 +72,69 @@ public class MyCanvas extends JPanel implements MouseListener {
     }
 
     @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (activeType != null) {
+            switch (activeType) {
+                case OVAL:
+                    g.setColor(activeColor);
+                    if (isFilled) {
+                        g.fillOval(activeFigure.getStart().x - 5, activeFigure.getStart().y - 5, 10, 10);
+                    } else {
+                        g.drawOval(activeFigure.getStart().x - 5, activeFigure.getStart().y - 5, 10, 10);
+                    }
+                case SQUARE:
+                    g.setColor(activeColor);
+                    if (isFilled) {
+                        g.fillRect(activeFigure.getStart().x - 5, activeFigure.getStart().y - 5, activeFigure.getFinish().x - 5, activeFigure.getFinish().y - 5);
+                    } else {
+                        g.drawRect(activeFigure.getStart().x - 5, activeFigure.getStart().y - 5, activeFigure.getFinish().x - 5, activeFigure.getFinish().y - 5);
+                    }
+            }
+        }
+    }
+
+    @Override
     public void mousePressed(java.awt.event.MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Mouse pressed.");
+        switch (activeType) {
+            case SQUARE:
+                activeFigure.setStart(e.getPoint());
+                this.repaint();
+        }
+
     }
 
     @Override
     public void mouseReleased(java.awt.event.MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Mouse released.");
+        switch (activeType) {
+            case SQUARE:
+                activeFigure.setFinish(e.getPoint());
+                this.repaint();
+        }
     }
 
     @Override
     public void mouseEntered(java.awt.event.MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Ratón pulsado.");
     }
 
     @Override
     public void mouseExited(java.awt.event.MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Ratón pulsado.");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Mouse clicked.");
+        switch (activeType) {
+            case OVAL:
+                activeFigure.setStart(e.getPoint());
+                activeFigure.setFinish(e.getPoint());
+                this.repaint();
+        }
+
     }
 
 }
